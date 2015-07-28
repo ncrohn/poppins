dd59a8524dee9821671fd12a7269690af9035011
Nick Crohn <nick.crohn@gmail.com>
2015-07-27 19:50:00 -0700

    Testing a different script

--- server.py
+++ server.py
@@ -1,28 +1,21 @@
 import socket
-import subprocess
+import time
+import picamera
 
-# Start a socket listening for connections on 0.0.0.0:8000 (0.0.0.0 means
-# all interfaces)
-server_socket = socket.socket()
-server_socket.bind(('0.0.0.0', 8000))
-server_socket.listen(0)
+with picamera.PiCamera() as camera:
+    camera.resolution = (640, 480)
+    camera.framerate = 24
 
-# Accept a single connection and make a file-like object out of it
-connection = server_socket.accept()[0].makefile('rb')
-try:
-    # Run a viewer with an appropriate command line. Uncomment the mplayer
-    # version if you would prefer to use mplayer instead of VLC
-    cmdline = ['vlc', '--demux', 'h264', '-']
-    #cmdline = ['mplayer', '-fps', '25', '-cache', '1024', '-']
-    player = subprocess.Popen(cmdline, stdin=subprocess.PIPE)
-    while True:
-        # Repeatedly read 1k of data from the connection and write it to
-        # the media player's stdin
-        data = connection.read(1024)
-        if not data:
-            break
-        player.stdin.write(data)
-finally:
-    connection.close()
-    server_socket.close()
-    player.terminate()
+    server_socket = socket.socket()
+    server_socket.bind(('0.0.0.0', 8000))
+    server_socket.listen(0)
+
+    # Accept a single connection and make a file-like object out of it
+    connection = server_socket.accept()[0].makefile('wb')
+    try:
+        camera.start_recording(connection, format='h264')
+        camera.wait_recording(60)
+        camera.stop_recording()
+    finally:
+        connection.close()
+        server_socket.close()
