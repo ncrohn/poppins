525c107e482d2563c7a98a56e3daa17a33f93459
Nick Crohn <nick.crohn@gmail.com>
2015-08-16 11:52:57 -0700

    Trying picam and opencv

--- lib/poppins/camera.py
+++ lib/poppins/camera.py
@@ -1,5 +1,7 @@
 # camera.py
 
+import io
+import picamera
 import cv2
 
 class VideoCamera(object):
@@ -7,18 +9,23 @@ class VideoCamera(object):
         # Using OpenCV to capture from device 0. If you have trouble capturing
         # from a webcam, comment the line below out and use a video file
         # instead.
-        self.video = cv2.VideoCapture(0)
+        #self.video = cv2.VideoCapture(0)
         # If you decide to use video.mp4, you must have this file in the folder
         # as the main.py.
         # self.video = cv2.VideoCapture('/Users/nickcrohn/Desktop/KarmiePrint.mp4')
 
-    def __del__(self):
-        self.video.release()
+        # Create the in-memory stream
+        stream = io.BytesIO()
+        with picamera.PiCamera() as camera:
+            camera.start_preview()
+            time.sleep(2)
+            camera.capture(stream, format='jpeg')
+            # Construct a numpy array from the stream
+            self.__data = np.fromstring(stream.getvalue(), dtype=np.uint8)
 
     def get_frame(self):
-        success, image = self.video.read()
+        image = cv2.imdecode(self.__data, 1)
         # We are using Motion JPEG, but OpenCV defaults to capture raw images,
         # so we must encode it into JPEG in order to correctly display the
         # video stream.
-        ret, jpeg = cv2.imencode('.jpg', image)
-        return jpeg.tobytes()
+        return image[:, :, ::-1]
